cff7eb184f7090848091809771b94837c2f1a70f
Sudheer Tangellapelly <tangellapelly@gmail.com>
2017-11-06 14:31:50 +0530

    removed sudheer from title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel sudheer</title>
-
+        <title>Laravel</title>
+    
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
